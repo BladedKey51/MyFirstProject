e50a1b9480ebe6e3983ec8558d484d0d7be02ffc
BladedKey51 <lanepopp123@gmail.com>
2024-08-29 10:39:27 -0500

    My first upload of a file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Lane Jones-Popp
+Computer Science Fall 2024
+Sept. 3, 24
+Lab 1, lab 1 intro to C++ Programming 
+This is the first lab and I followed the instruction to the letter and it helped me learn how to 
+make new projects and other important things like making comments. Basic demo of C++ language.
+*/
+
+#include <iostream>
+
+int main()
+
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Light Blue" << std::endl;
+
+	return 0;
+}
